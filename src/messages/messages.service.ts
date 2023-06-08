0038095af1e531679d33498c858a1ca7d822ab89
Vi Nguyen <khanhvi2941@gmail.com>
2023-06-08 18:05:45 +0700

    build api remove message

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
-import { Message } from './schemas/message.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Message } from './schemas/message.schema';
 
 @Injectable()
 export class MessagesService {
@@ -13,22 +12,10 @@ export class MessagesService {
 
   async create(createMessageDto: CreateMessageDto): Promise<Message> {
     const createMessage = new this.messageModel(createMessageDto);
-    return createMessage.save();
+    return await createMessage.save();
   }
 
-  findAll() {
-    return `This action returns all messages`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} message`;
-  }
-
-  update(id: number, updateMessageDto: UpdateMessageDto) {
-    return `This action updates a #${id} message`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} message`;
+  async remove(id: string) {
+    return await this.messageModel.findByIdAndDelete(id);
   }
 }
