44e9d84c1af575f53567dd057ac50e4e6cef0cb5
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-06-11 19:12:37 +0700

    chore(rooms): update CreateRoomDto properties

--- src/rooms/dto/create-room.dto.ts
+++ src/rooms/dto/create-room.dto.ts
@@ -1,9 +1,9 @@
 import { Schema } from 'mongoose';
 
 export class CreateRoomDto {
-  readonly participant: Schema.Types.ObjectId[];
+  readonly participants: Schema.Types.ObjectId[];
   readonly isGroup: boolean;
   readonly admin: Schema.Types.ObjectId;
-  readonly avatar: string;
-  readonly name: string;
+  readonly avatar?: string;
+  readonly name?: string;
 }
